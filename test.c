752cd3520ace0127e07c34fc05286bb5fc948ed3
Frank Collins <fwc3rd@gmail.com>
2023-04-30 13:47:13 -0400

    {array: initialized " " || array; } [1:47pm]
    initialize array to be empty strings
    before I was just declaring the arrays without any value assignment

--- test.c
+++ test.c
@@ -1,62 +1,78 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
 
-int main() {
-    FILE *fp1, *fp2, *fp3;
+#define MAX_USER_LENGTH 20
+
+int main () {
+
+    FILE  *fp1, *fp2, *fp3;
     char ch;
 
-    // Open input files
-    fp1 = fopen("/Users/medium/Desktop/patrickbetdavid1.txt", "r");
+    char user[MAX_USER_LENGTH];
+    char file1[260];
+    char file2[260];
+    char filepath_1[100] = "/Users";
+    char filepath_2[100];
+    char userpath[100] = "/Users/";
+    char desktop_path[100] = "";
+    char desktop_path_2[100] = "";
+    char prefilepath[100];
+
+    printf("Please enter your username: ");
+    fgets(user, sizeof(user), stdin);
+    user[strcspn(user, "\n")] = '\0';
+
+    printf("My PC/Mac username is: %s\n", user);
+
+    printf("Please enter the name of the first file you'd like to open and merge: ");
+    fgets(file1, sizeof(file1), stdin);
+    file1[strcspn(file1, "\n")] = '\0';
+
+    strcat(userpath, user);
+    strcat(userpath, "/Desktop/");
+    printf("userpath:\t %s \n", userpath);
+
+    strcat(desktop_path, "/");
+    strcat(desktop_path, userpath);
+    printf("desktop_path:\t %s \n", desktop_path);
+
+    strcat(userpath, file1);
+
+    printf("heres my filepath:\t %s \n", userpath);
+
+    fp1 = fopen(userpath, "r");
     if (fp1 == NULL) {
-        printf("Error: unable to open input file patrickbetdavid1.txt.\n");
-        return 1;
-    }
-    
-    fp2 = fopen("/Users/medium/Desktop/patrickbetdavid2.txt", "r");
-    if (fp2 == NULL) {
-        printf("Error: unable to open input file patrickbetdavid2.txt.\n");
-        fclose(fp1);
+        printf("Error: unable to open file %s.\n", file1);
         return 1;
     }
 
-    // Open output file for writing
-    fp3 = fopen("/Users/medium/Desktop/merged_patrickbetdavid.txt", "w");
-    if (fp3 == NULL) {
-        printf("Error: unable to create output file merged_patrickbetdavid.txt.\n");
-        fclose(fp1);
-        fclose(fp2);
-        return 1;
-    }
-
-    // DOCTOR DOCTOR!
-    // Copy contents of first file to output file
     while ((ch = fgetc(fp1)) != EOF) {
-        if (fputc(ch, fp3) == EOF) {
-            printf("Error: failed to write to output file.\n");
-            fclose(fp1);
-            fclose(fp2);
-            fclose(fp3);
-            return 1;
-        }
+        printf("%c", ch);        
     }
 
-    // DOCTOR DOCTOR! Dont forget to patch 'em up!
-    // Copy contents of second file to output file
-    while ((ch = fgetc(fp2)) != EOF) {
-        if (fputc(ch, fp3) == EOF) {
-            printf("Error: failed to write to output file.\n");
-            fclose(fp1);
-            fclose(fp2);
-            fclose(fp3);
-            return 1;
-        }
-    }
+    printf("\n"); printf("\n");
 
-    // Close all files
-    fclose(fp1);
-    fclose(fp2);
-    fclose(fp3);
+    printf("Enter file2 with its file extension into input: ");
+    fgets(file2, sizeof(file2), stdin);    
+    strcat(filepath_2, desktop_path);
+    strcat(filepath_2, file2);
+
+    printf("my filepath2:\t %s\n", filepath_2);
+
+    fp2 = fopen(filepath_2, "r");
+    if (fp2 == NULL) {
+        printf("Error: unable to open file %s.\n", file2);
+        return 1;
+    }
     
-    printf("Files merged successfully.\n");
+    while ((ch = fgetc(fp2)) != EOF) {
+        printf("%c", ch);        
+    }
+
     return 0;
 }
