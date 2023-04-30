2f37e30ecd0cf28286f5170fbdeb987f1675e632
Frank Collins <fwc3rd@gmail.com>
2023-04-30 11:19:07 -0400

    fix issue #5) thanks to: https://www.youtube.com/watch?v=Si8rN5J249M [11:19am]

--- md.c
+++ md.c
@@ -1,5 +1,10 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/ioctl.h>
 
     #define MAX_USER_LENGTH 20
 
@@ -9,57 +14,56 @@ int main () {
     char ch;
 
     char user[MAX_USER_LENGTH];
-    scanf(" %[^\n]s", user);
-    
-    fp1 = fopen("/Users/medium/Desktop/patrickbetdavid1.txt", "r");
-    if (fp1 == NULL) {
-        printf("Error: unable to open file patrickbetdavid1.txt.\n");
-        return 1;
-    }
 
-        while ((ch = fgetc(fp1)) != EOF) {
-                    printf("%c", ch);        
-        }
-    fp2 = fopen("/Users/medium/Desktop/patrickbetdavid2.txt", "r");
-    if (fp2 == NULL) {
-        printf("Error: unable to open file patrickbetdavid2.txt.\n");
-        return 1;
-    }
+    printf("please enter your username: \n");
     
-        while ((ch = fgetc(fp2)) != EOF) {
-                    printf("%c", ch);        
-        }
-
-    fp3 = fopen("/Users/medium/Desktop/merged_patrickbetdavid.txt", "w"); // open new file for writing
-    if (fp3 == NULL) {
-        printf("Error: unable to create file merged_patrickbetdavid.md.\n");
-        return 1;
-    } else {
-        remove("/Users/medium/Desktop/patrickbetdavid1.txt");
-        remove("/Users/medium/Desktop/patrickbetdavid2.txt");
-    }
+    fgets(user, sizeof(user), stdin);
+    printf("my PC | mac username is: %s \n", user);
     
-    // copy contents of first file to new file
-    while ((ch = fgetc(fp1)) != EOF) {
-        fputc(ch, fp3);
-    }
-    
-    // copy contents of second file to new file
-    while ((ch = fgetc(fp2)) != EOF) {
-        fputc(ch, fp3);
-    }
-    
-    // close all files
-    fclose(fp1);
-    fclose(fp2);
-    fclose(fp3);
-    
-    // delete original files
-    // if (remove("patrickbetdavid1.md") == 0 && remove("patrickbetdavid2.md") == 0) {
-    //     printf("Files merged successfully and originals deleted.\n");
-    // } else {
-    //     printf("Error: unable to delete original files.\n");
+    // fp1 = fopen("/Users/medium/Desktop/patrickbetdavid1.txt", "r");
+    // if (fp1 == NULL) {
+    //     prisntf("Error: unable to open file patrickbetdavid1.txt.\n");
+    //     return 1;
     // }
+
+    //     while ((ch = fgetc(fp1)) != EOF) {
+    //                 printf("%c", ch);        
+    //     }
+    // fp2 = fopen("/Users/medium/Desktop/patrickbetdavid2.txt", "r");
+    // if (fp2 == NULL) {
+    //     printf("Error: unable to open file patrickbetdavid2.txt.\n");
+    //     return 1;
+    // }
+    
+    //     while ((ch = fgetc(fp2)) != EOF) {
+    //                 printf("%c", ch);        
+    //     }
+
+    // fp3 = fopen("/Users/medium/Desktop/merged_patrickbetdavid.txt", "w"); // open new file for writing
+    // if (fp3 == NULL) {
+    //     printf("Error: unable to create file merged_patrickbetdavid.md.\n");
+    //     return 1;
+    // } else {
+    //     remove("/Users/medium/Desktop/patrickbetdavid1.txt");
+    //     remove("/Users/medium/Desktop/patrickbetdavid2.txt");
+    // }
+    
+    // // copy contents of first file to new file
+    // while ((ch = fgetc(fp1)) != EOF) {
+    //     fputc(ch, fp3);
+    // }
+    
+    // // copy contents of second file to new file
+    // while ((ch = fgetc(fp2)) != EOF) {
+    //     fputc(ch, fp3);
+    // }
+    
+    // // close all files
+    // fclose(fp1);
+    // fclose(fp2);
+    // fclose(fp3);
+    
+
 
     return 0;
 }
